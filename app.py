8eba43da8be81080ae3bcb9f2dab2b454096ee85
Jessica Phillip <jessicaophillip@gmail.com>
2020-09-15 13:13:09 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,33 +1,11 @@
-# import modules
+# IMPORTS
 import pandas as pd
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 import pickle
-from pickle import load
-import os, sys
-import numpy as np
-import pandas as pd
-from keras.models import model_from_json
-from keras.models import model_from_yaml
-import json
-
-# LOAD MODELS
-# yaml_file = open('model_files/model.yaml', 'rb')  # load YAML and create model         
-# loaded_model_yaml = yaml_file.read() 
-# yaml_file.close()
-# loaded_model = model_from_yaml(loaded_model_yaml) 
-# print("Loaded model from disk w/ YAML")
-
-# loaded_model.load_weights('model_files/model.h5') # load weights into new model       
-# print("Loaded model from disk w/ HdF5")
-
-# json_file = open('model_files/model.json', 'rb')  # load json and create model
-# loaded_model_json = json_file.read()
-# json_file.close()
-# loaded_model = model_from_json(loaded_model_json)
-# print("Loaded model from disk w/ JSON")
+import flask_monitoringdashboard as dashboard
 
 # LOAD MODEL
-model = pickle.load(open('model.pkl','rb')) # TT
+model = pickle.load(open('model.pkl','rb'))
 
 # CREATE API ROUTE
 app = Flask(__name__)
@@ -38,36 +16,30 @@ app = Flask(__name__)
 # PREDICTION MODEL
 def predict():
 
-    # Converts incoming request data from JSON object into Python data
-    # data = request.get_json(force=False)  # Ignore the mimetype and always try to parse data as JSON.
-    data = request.get_json() ## CC = Removed (force = False)
-    print(type(data), data)
+    # We can read the content of the server’s response. 
+    # Response Content¶
+    # get data
+    data = request.get_json() # Converts incoming request data from JSON object into Python data   
+    
+    # request.get_json() converts the JSON object into Python data for us. JSON decoder
+    # Let's assign the incoming request data to variables and return them 
+    # by making the following changes to our json-example route.
 
-    data = json.loads(data)          # load str into dict to use it
-
-   # convert json data from dict into dataframe
+    # convert json data from dict into dataframe
     data.update((x, [y]) for x, y in data.items()) # Insert an item to the dict using .update()
     data_df = pd.DataFrame.from_dict(data)         # Convert dict to dataframe
 
-    # data_arr = np.array(data_df)                          # Convert data to array
-    # print("Convert sample to array", data_arr)
+    # predictions
+    result = model.predict(data_df)
 
-    # Transform Data from 2D to 3D for CNN 
-    # data_arr = data_arr.reshape(data_arr.shape[0], data_arr.shape[1], 1)
+    # send back to browser
+    output = {'results': int(result[0])}
 
-    # Predict using model
-    # yTween = loaded_model.predict(data_arr)
-    yTween = model.predict(data_df)
-    print("Tween prediced values: unscaled , complete.")
-
-    #the curly brackets turn object into dict
-    output = {'results': int(yTween[0])} # Added:  output = {'results': int(result[0])}
-    print(output, type(output))
-
+    # return data
     return jsonify(results=output)
 
-if __name__ == '__main__': 
-    app.run(port = 5000, debug=True) # Added: app.run(port = 5000, debug=True)
 
 
+if __name__ == '__main__':
+    app.run(port = 5000, debug=True)
 
