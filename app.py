71af0d67646cc48af1e2b6da56a0f6f57c0a48f8
Jessica Phillip <jessicaophillip@gmail.com>
2020-09-15 13:02:25 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -2,12 +2,12 @@
 import pandas as pd
 from flask import Flask, request, jsonify
 import pickle
+from pickle import load
 import os, sys
 import numpy as np
 import pandas as pd
 from keras.models import model_from_json
 from keras.models import model_from_yaml
-from pickle import load
 import json
 
 # LOAD MODELS
@@ -26,23 +26,26 @@ import json
 # loaded_model = model_from_json(loaded_model_json)
 # print("Loaded model from disk w/ JSON")
 
+# LOAD MODEL
 model = pickle.load(open('model.pkl','rb')) # TT
 
 # CREATE API ROUTE
 app = Flask(__name__)
 
+# API ROUTE
 @app.route('/', methods=['POST'])
 
 # PREDICTION MODEL
 def predict():
 
     # Converts incoming request data from JSON object into Python data
-    data = request.get_json(force=False)   # Ignore the mimetype and always try to parse data as JSON.
+    # data = request.get_json(force=False)  # Ignore the mimetype and always try to parse data as JSON.
+    data = request.get_json() ## CC = Removed (force = False)
     print(type(data), data)
 
     data = json.loads(data)          # load str into dict to use it
 
-    # Convert Json data to dict then to array to be processed
+   # convert json data from dict into dataframe
     data.update((x, [y]) for x, y in data.items()) # Insert an item to the dict using .update()
     data_df = pd.DataFrame.from_dict(data)         # Convert dict to dataframe
 
@@ -57,13 +60,14 @@ def predict():
     yTween = model.predict(data_df)
     print("Tween prediced values: unscaled , complete.")
 
-    output = {'results': (yTween)} #the curly brackets turn object into dict
+    #the curly brackets turn object into dict
+    output = {'results': int(yTween[0])} # Added:  output = {'results': int(result[0])}
     print(output, type(output))
 
     return jsonify(results=output)
 
 if __name__ == '__main__': 
-    app.run(port = 3000, debug=True)
+    app.run(port = 5000, debug=True) # Added: app.run(port = 5000, debug=True)
 
 
 
